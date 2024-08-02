7ed7b29f82e3be9da2c7d4103c213e3bbe423a9f
Abdullah Al Suad <suadabdullahal@gmail.com>
2024-08-02 11:05:15 +0600

    hero  done

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    
-    <main className="relative bg-black-100 flex justify-center items-start flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <Hero />
-      </div>
-    </main>
-
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <div className="max-w-7xl w-full">
+      <Hero />
+    </div>
+  </main> 
   );
 }
